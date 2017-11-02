7401af7be4664bda9395bdf1ae7051123b02c40b
Marcel <Klatuu82@users.noreply.github.com>
2017-11-02 15:53:17 +0100

    Add files via upload

--- session_login.php
+++ session_login.php
@@ -20,57 +20,70 @@ function showLoginWindow($status)
 }
 
 //>>>>
-if(!isset($_SESSION['username']) && empty($_POST))
+if(isset($_SESSION['username'])) 
+{
+   //       createAdminWindow();
+  $name = $_SESSION['username'];
+  $permission = $_SESSION['permission'];
+  
+  if($permission == "admin")
+  {
+    echo "Willkommen ADMIN {$_SESSION['username']}<br >";
+    //header('location:admin.php');
+  }else if($permission == "editor")
+  {
+    echo "Willkommen EDITOR {$_SESSION['username']}<br >";
+    //header('location:editor.php');
+  }
+}else if(empty($_POST['pwd']))
 {
   showLoginWindow(0);      
-}
-
-if(!isset($_SESSION['username']) && !empty($_POST)) 
+}else
 {
-$name = $_POST['name'];
-$pwd = $_POST['pwd'];
+  $name = $_POST['name'];
+  $pwd = $_POST['pwd'];
   //bekomme POST von HTML Login fenster
   $table = "t_user";
   $verbindung = mysqli_connect("localhost", "root", "", "adWords");
 
   //suche benutzer und passendes passwort
-  $sql = "SELECT pwd FROM $table WHERE user='$name'";
+  $sql = "SELECT pwd, permission FROM $table WHERE name='$name'";
   $rows = mysqli_query($verbindung, $sql);
-
-
-if($rows > 0) 
-{
-  //benutzer gefunden
-  $db_pwd = mysql_fetch_assoc($rows);
   
-  /*
-  $verschluesselung = hash('sha256', $pwd);
-  */
   
-  // geheim: addb0f5e7826c857d7376d1bd9bc33c0c544790a2eac96144a8af22b1298c940
-
-  //passwort verschluesseln und mit passwort in db vergleichen
-  if(hash('sha256', $pwd) == $db_pwd['pwd']) {
-        //anmeldung erfolgreich
-        $_SESSION['username'] = $name;
-        echo "Hallo $name";
-//        createAdminWindow();
+  if($rows != "") 
+  {
+    //benutzer gefunden
+    $db_pwd = mysqli_fetch_assoc($rows);
+    //echo "DB: {$db_pwd['pwd']} <br >";
+    //echo "Eingabe: ".hash('sha256', $pwd)."<br >";
+    /*
+    $verschluesselung = hash('sha256', $pwd);
+    */
+    
+    // geheim: addb0f5e7826c857d7376d1bd9bc33c0c544790a2eac96144a8af22b1298c940
+  
+    //passwort verschluesseln und mit passwort in db vergleichen
+    if(hash('sha256', $pwd) == $db_pwd['pwd']) 
+    {
+          //anmeldung erfolgreich
+          $_SESSION['username'] = $name;
+          $_SESSION['permission'] = $db_pwd['permission'];
+          header('location:geht.php');
+          exit;
+    //createAdminWindow();
+      
+    }
+    else 
+    {
+        showLoginWindow(1);  
+    }
+    
+  mysqli_close($verbindung);
   }
   else
   {
-      showLoginWindow(1);
-
+       showLoginWindow(1);
   }
-  
-  if(isset($_SESSION['username'])) {
- //       createAdminWindow();
-      $name = $_SESSION['username'];
-       echo "Willkommen zur&uuml;ck $name";
-  }
-}
-else
-{
-     showLoginWindow(1);
-}
 }
 ?>
