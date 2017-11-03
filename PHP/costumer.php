89d1134cab8aeb9b9261168af5be54ae47129674
raslan3333 <raslan3333@gmail.com>
2017-11-03 16:15:57 +0100

    Add files via upload

--- PHP/costumer.php
+++ PHP/costumer.php
@@ -1,101 +1,143 @@
 <!DOCTYPE html>
   <html>
     <head>
+      <style>
+
+      </style>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
-
       <div class="row">
-            <div class="col s12">This div is 12-columns wide on all screen sizes</div>
-            <div class="col s6" style="margin:50px;margin-top:100px; width:50%;height:400px;overflow: scroll;">
-              <ul class="collapsible" data-collapsible="accordion">
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          <div class="col s8" style="margin:0px;margin-top:100px;">
+              <ul class="collapsible" data-collapsible="accordion" style="width:100%;height:400px;overflow: scroll;">
+                  <li >
+                    <div class="collapsible-header"><b>Admin</b></div>
+                     <div class="collapsible-body">
+                      <span>
+                       <table style="margin:0px;padding:0px">
+                          <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>text</th>
+                                <th>pic_Link</th>
+                                <th>e_Mail</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                               <td style="margin:0px;padding:0px">
+                                 <form >
+                                   <div class="input-field " style="margin:0px;">
+                                      <input  type="text" class="validate" style="width:90%;">
+                                      <label >Name</label>
+                                  </div>
+                                </form>
+                              </td>
+                                  <td>
+                                    <div class="input-field " >
+                                     <textarea id="textarea1" class="materialize-textarea" data-length="1024" style="width:90%;max-height:75px;overflow:scroll;overflow-x: hidden;">
+                                     hallo welt
+                                     </textarea>
+                                     <label for="textarea1">Text</label>
+                                   </div>
+                                </td>
+                                <td>
+                                  <div class="row">
+                                    <form >
+                                        <div class="input-field ">
+                                          <input  type="text" class="validate" style="width:90%;" data-length="1024">
+                                          <label >Foto</label>
+                                      </div>
+                                    </form>
+                                  </div>
+
+                                </td>
+                                <td>
+                                  <div class="row">
+                                    <form >
+                                        <div class="input-field ">
+                                          <input id="email" type="email" class="validate" style="width:90%;">
+                                          <label >Email</label>
+                                      </div>
+                                    </form>
+                                  </div>
+                                </td>
+                                </tr>
+                         </tbody>
+                      </table>
+                     </span>
+                   </div>
                   </li>
                 </ul>
-
-            </div>
-            
           </div>
-          <div class="row">
-            <div class="col s6" style="margin:50px;margin-top:100px; width:50%;height:400px;overflow: scroll;">
-              <ul class="collapsible" data-collapsible="accordion">
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                </ul>
+       </div>
+       <div class="row">
+           <div class="col s8" style="margin:0px;margin-top:100px;">
+               <ul class="collapsible" data-collapsible="accordion" style="width:100%;height:400px;overflow: scroll;">
+                   <li >
+                     <div class="collapsible-header"><b>Benutzer</b></div>
+                      <div class="collapsible-body">
+                       <span>
+                        <table style="margin:0px;padding:0px">
+                           <thead>
+                             <tr>
+                                 <th>Name</th>
+                                 <th>text</th>
+                                 <th>pic_Link</th>
+                                 <th>e_Mail</th>
+                             </tr>
+                           </thead>
+                           <tbody>
+                             <tr>
+                                <td style="margin:0px;padding:0px">
+                                  <form >
+                                    <div class="input-field " style="margin:0px;">
+                                       <input  type="text" class="validate" style="width:90%;">
+                                       <label >Name</label>
+                                   </div>
+                                 </form>
+                               </td>
+                                   <td>
+                                     <div class="input-field " >
+                                      <textarea id="textarea1" class="materialize-textarea" data-length="1024" style="width:90%;max-height:75px;overflow:scroll;overflow-x: hidden;">
+                                      hallo welt
+                                      </textarea>
+                                      <label for="textarea1">Text</label>
+                                    </div>
+                                 </td>
+                                 <td>
+                                   <div class="row">
+                                     <form >
+                                         <div class="input-field ">
+                                           <input  type="text" class="validate" style="width:90%;" data-length="1024">
+                                           <label >Foto</label>
+                                       </div>
+                                     </form>
+                                   </div>
 
-            </div>
+                                 </td>
+                                 <td>
+                                   <div class="row">
+                                     <form >
+                                         <div class="input-field ">
+                                           <input id="email" type="email" class="validate" style="width:90%;">
+                                           <label >Email</label>
+                                       </div>
+                                     </form>
+                                   </div>
+                                 </td>
+                                 </tr>
+                          </tbody>
+                       </table>
+                      </span>
+                    </div>
+                   </li>
+                 </ul>
+           </div>
+        </div>
 
-              </div>
 
 
 
